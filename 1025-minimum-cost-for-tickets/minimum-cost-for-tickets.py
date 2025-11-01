69569e3beff47e7aa4c78dc9ca79b4f8dc29abd3
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-11-01 00:06:52 -0700

    Time: 7 ms (37.23%) | Memory: 18 MB (49.37%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,39 +1,22 @@
 class Solution:
-    # Top Down
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = {}
+        memo = {}
         n = len(days)
-        dp[n] = 0
 
-        def dfs(index):
-            if index in dp:
-                return dp[index]
+        def backtrack(i):
+            if i >= n:
+                return 0
+            
+            if i in memo:
+                return memo[i]
 
-            dp[index] = sys.maxsize
-            k = index
-            for cost, duration in zip(costs, [1,7,30]):
-                while k < len(days) and days[k] - days[index] < duration :
+            memo[i] = sys.maxsize
+
+            k = i 
+            for cost, duration in zip(costs, [1, 7, 30]):
+                while k < n and days[k] - days[i] < duration:
                     k += 1
-                dp[index] = min(dp[index], dfs(k) + cost)
-            return dp[index]
+                memo[i] = min(memo[i], backtrack(k) + cost)
+            return memo[i]
         
-        return dfs(0)
-            
-    # Bottom Up
-    # def mincostTickets1(self, days: List[int], costs: List[int]) -> int:
-    #     n = len(days)
-    #     dp = [0] * (n + 1)
-    #     for i in range(1, n + 1):
-    #         dp[i] = dp[i - 1] + costs[0]
-
-    #         j = i - 1
-    #         while j > 0 and days[i - 1] - days[j - 1] < 7:
-    #             j -= 1
-    #         dp[i] = min(dp[i], dp[j] + costs[1])
-            
-    #         k = i - 1
-    #         while k > 0 and days[i - 1] - days[k - 1] < 30:
-    #             k -= 1
-    #         dp[i] = min(dp[i], dp[k] + costs[2])
-    #     return dp[n]
-            
+        return backtrack(0)            
