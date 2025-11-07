c5141bb0df8eb37b360a881489b1d08564d4afe0
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-11-06 19:18:51 -0800

    Time: 35 ms (5.31%) | Memory: 20.9 MB (15.22%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def count_min(self, intervals: List[List[int]]) -> int:
+        q = deque(intervals)
+        count = 0
+        while q:
+            print(count, q)
+            start, end = q.popleft()
+            temp_q = deque()
+            count += 1
+            for _ in range(len(q)):
+                print("here")
+                curr_s, curr_e = q.popleft()
+                if end <= curr_s:
+                    start, end = curr_s, curr_e
+                else:
+                    temp_q.append((curr_s, curr_e))
+            q = temp_q
+        return count
+
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:        
+        if len(intervals) == 1:
+            return 1
+        
+        intervals_end = sorted(intervals, key=lambda x: x[1]) # sorting on end times
+        intervals_start = sorted(intervals)
+        return min(self.count_min(intervals_start), self.count_min(intervals_end))
+        
+
+
