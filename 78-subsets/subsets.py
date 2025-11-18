4279d1089a684c59e977fcc9faad54201fdf7295
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-11-17 20:55:26 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.61%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        retVal = [[]]
-        prev_answer = [[i] for i in nums]
-        retVal.extend(prev_answer)
-        print(retVal)
         n = len(nums)
-        for i in range(n - 1):
-            current = []
-            for k in prev_answer:
-                print(k)
-                for item in nums:
-                    if item > k[-1]:
-                        current.append(k[::] + [item])  
-            retVal.extend(current)
-            prev_answer = current
-        return retVal
+
+        results = []
+        subset = []
+
+        def helper(i: int):
+            if i == n:
+                results.append(subset[:])
+                return
+            
+            # choice
+            subset.append(nums[i])
+            helper(i + 1)
+            subset.pop()
+            helper(i + 1)
+        
+        helper(0)
+        return results
