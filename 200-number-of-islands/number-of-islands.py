63f4421dbff9353b6f9ffede3e4203aa16681269
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-11-20 22:01:41 -0800

    Time: 322 ms (7.74%) | Memory: 28.3 MB (10.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,24 @@
-from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[0,1], [1,0], [0,-1], [-1, 0]]
-        visited = set()
+        if not grid:
+            return 0
         rows, cols = len(grid), len(grid[0])
-        count = 0 
+        directions = [[0,1], [1, 0], [0, -1], [-1, 0]]
+        visited = set()
+        
+        def dfs(x,y):
+            visited.add((x,y))
 
-        def dfs(i,j):
-            # (i,j) not in visited all the time 
-            visited.add((i,j))
-
-            q = deque([(i,j)])
-            while q:
-                x,y = q.popleft()
-                for dx,dy in directions:
-                    X, Y = x+dx, y+dy
-                    if X in range(rows) and Y in range(cols) and (X,Y) not in visited:
-                        if grid[X][Y] == "1":
-                            q.append([X,Y])
-                        visited.add((X,Y))
-                    
+            for dx, dy in directions:
+                X,Y= x+dx, y+dy
+                if X in range(rows) and Y in range(cols) and grid[X][Y] == "1" and (X,Y) not in visited: 
+                    dfs(X,Y)
+        
+        count = 0
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1" and (i,j) not in visited:
                     dfs(i,j)
                     count += 1
+        
         return count
