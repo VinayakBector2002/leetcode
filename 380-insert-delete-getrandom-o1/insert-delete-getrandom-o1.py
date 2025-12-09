f99aaf136480d875cbdb0c3bfb42d1728000b0e1
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-12-08 22:11:22 -0800

    Time: 148 ms (40.62%) | Memory: 58.1 MB (19.93%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -0,0 +1,55 @@
+'''
+RandomizedSet() Initializes the RandomizedSet object.
+bool insert(int val) 
+    Inserts an item val into the set if not present. 
+    Returns true if the item was not present, false otherwise.
+
+bool remove(int val) 
+    Removes an item val from the set if present. 
+    Returns true if the item was present, false otherwise.
+
+int getRandom() 
+    Returns a random element from the current set of elements 
+    (it's guaranteed that at least one element exists when this method is called). 
+    Each element must have the same probability of being returned.
+
+You must implement the functions of the class such that each function works in average O(1) time complexity.
+'''
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.map = dict()
+        self.lst = list()
+
+    def insert(self, val: int) -> bool:
+        if val not in self.map:
+            self.map[val] = len(self.lst)
+            self.lst.append(val)
+            return True
+        # else
+        return False 
+
+    def remove(self, val: int) -> bool:
+        if val in self.map:
+            # perform swap operation
+            curr_last = self.lst[-1]
+            self.map[val], self.map[curr_last] = self.map[curr_last], self.map[val]
+            self.lst[self.map[val]], self.lst[self.map[curr_last]] = self.lst[self.map[curr_last]], self.lst[self.map[val]]
+            # val should be the last element now 
+            self.lst.pop()
+            del self.map[val]
+            return True
+        return False        
+
+    def getRandom(self) -> int:
+        index = random.randrange(len(self.lst))
+        return self.lst[index]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
