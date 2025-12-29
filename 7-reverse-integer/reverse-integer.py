8956e536ff893805f8f6ecebe5f59019ec6a5a7a
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-12-29 19:25:31 +0530

    Time: 41 ms (39.99%) | Memory: 17.3 MB (94.09%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        stack = list(str(x))
+        result = []
+        
+        # pop all the trailng zeros
+        while stack and stack[-1] == '0':
+            stack.pop()
+
+        if len(stack) == 0:
+            print(stack)
+            return 0
+        
+        while stack and stack[-1] != "-":
+            result.append(stack.pop())
+        
+        result = "".join(result)
+        result = int(f"-{result}") if stack else int(result)
+        
+        return result if result in range(-2**31, 2**31) else 0
