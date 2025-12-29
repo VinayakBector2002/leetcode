05d15656bed82ee4086ddb74b5ba1e8f8bb107be
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-12-29 19:17:15 +0530

    Time: 4 ms (65.63%) | Memory: 18.3 MB (99.72%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         target = len(graph) - 1
@@ -13,3 +14,26 @@ class Solution:
 
         dfs(0, [0])
         return res
+
+'''
+
+from collections import deque, defaultdict
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        q = deque()
+        q.append([0])
+        target =  len(graph) - 1
+        paths = []
+        
+        def dfs():
+            while q:
+                currentPath = q.pop()
+                for nbr in graph[currentPath[-1]]:    
+                    newPath = currentPath + [nbr] 
+                    if nbr == target:
+                        paths.append(newPath)
+                    else:
+                        q.append(newPath)
+        dfs()
+        return paths
