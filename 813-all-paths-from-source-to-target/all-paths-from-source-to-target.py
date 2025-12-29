8eb3c54b8b8d3180cac8ef1e21b4788974b2f1db
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2025-12-29 19:16:41 +0530

    Time: 4 ms (65.63%) | Memory: 18.7 MB (93.05%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,23 +1,15 @@
-from collections import deque, defaultdict
-from copy import deepcopy
-
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        q = deque()
-        q.append([0])
-        target =  len(graph) - 1
-        paths = []
-        
-        def dfs():
-            while q:
-                currentPath = q.pop()
-                for nbr in graph[currentPath[-1]]:    
-                    copyPath = deepcopy(currentPath)
-                    copyPath.append(nbr)
+        target = len(graph) - 1
+        res = []
 
-                    if nbr == target:
-                        paths.append(copyPath)
-                    else:
-                        q.append(copyPath)
-        dfs()
-        return paths
+        def dfs(node, path):
+            if node == target:
+                res.append(path)
+                return
+            
+            for nbr in graph[node]:
+                dfs(nbr, path + [nbr])
+
+        dfs(0, [0])
+        return res
